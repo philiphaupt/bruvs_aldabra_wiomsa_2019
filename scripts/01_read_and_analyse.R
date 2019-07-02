a466f4188d42f04c6ffabfa5003fe667d6e1930d
Philip Haupt <philip.haupt@gmail.com>
2019-07-02 16:04:05 +0100

    1st upload of analysis script for WIOMSA

---
+++ scripts/01_read_and_analyse.R
@@ -0,0 +1,46 @@
+library(tidyverse)
+
+
+#read BRUVs data
+dat_full <- read_rds("E:/stats/aldabra/BRUVs/01_prep_data/04_size_classes_2_from_chapter_4/fish.df4.RDS")
+bruvs_dat <- dat_full %>%
+        select(opcode,
+               depth,
+               treatment = comment,
+               class = Class,
+               order = Order,
+               family,
+               maxn,
+               species_name = vsppname,
+               pres_abs = pres.abs) #%>%
+        #filter(treatment == "baited") 
+               
+
+#number of baited samples - for checking results (result = 115 baited samples)
+bruvs_dat %>% distinct(opcode)
+
+# make sure that class is a factor - this will allow the .frop argument to work properly - and not remove zero counts
+bruvs_dat$class <- as.factor(bruvs_dat$class)
+
+#summary of elasmobranchs in BRUVs
+sample_summary = bruvs_dat %>% group_by(opcode,
+                                        treatment,#to make sure that all samples are included
+                                        class) %>% #to sepearate results between bony fish and alsmobranchs
+                       summarise(sum_maxn = sum(maxn),
+                                 sum_pres = sum(pres_abs)
+                       )
+
+# obtain zero counts where there were absences!
+complete_sample_summary <- sample_summary %>% complete(class, nesting(
+                                                       opcode),
+                                                       fill = list(sum_maxn = 0,
+                                                                   sum_pres = 0))
+
+class_summary <- complete_sample_summary %>%
+        group_by(treatment,
+                 class) %>%
+        summarise(mean_maxn = mean(sum_maxn))
+
+#class == "Elasmobranchii")
+               
+               
